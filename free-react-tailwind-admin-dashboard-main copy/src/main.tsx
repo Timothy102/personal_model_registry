32b02a05df841283578e2ef82e0993af098d1df4
Tim <cvetko.tim@gmail.com>
2024-04-14 10:21:06 +0200

    removed auth0

--- free-react-tailwind-admin-dashboard-main copy/src/main.tsx
+++ free-react-tailwind-admin-dashboard-main copy/src/main.tsx
@@ -1,17 +1,16 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { Auth0Provider, useAuth0, withAuthenticationRequired } from '@auth0/auth0-react';
 import App from './App';
 import './css/style.css';
 import 'jsvectormap/dist/css/jsvectormap.css';
 import 'flatpickr/dist/flatpickr.min.css';
 
-
 ReactDOM.render(
   <React.StrictMode>
-    
+    <Router>
       <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
